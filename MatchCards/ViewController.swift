d7f3f51ed88f7ff54e84c17c431ecd3824f6d134
shehzad <shehzadkhuwaja@gmail.com>
2023-11-14 01:11:11 +0500

    added score and flip count label

--- MatchCards/ViewController.swift
+++ MatchCards/ViewController.swift
@@ -23,12 +23,33 @@ class ViewController: UIViewController {
     }
     
     func createCardUI(numberOfRows: Int, cardsPerRow: Int) {
-        let mainStackView = UIStackView()
-        mainStackView.axis = .vertical
-        mainStackView.alignment = .fill
-        mainStackView.distribution = .fillEqually
-        mainStackView.spacing = 10
+        // Create a stack view for the top section
+        let topStackView = UIStackView()
+        topStackView.axis = .horizontal
+        topStackView.alignment = .fill
+        topStackView.distribution = .fillProportionally
+        topStackView.spacing = 10
         
+        // Create the score label and flip count label
+        let scoreLabel = UILabel()
+        scoreLabel.text = "Score: 0"
+        scoreLabel.textAlignment = .left
+        let flipCountLabel = UILabel()
+        flipCountLabel.text = "Flips: 0"
+        flipCountLabel.textAlignment = .right
+        
+        // Add the labels to the top stack view
+        topStackView.addArrangedSubview(scoreLabel)
+        topStackView.addArrangedSubview(flipCountLabel)
+        
+        // Create a stack view for the bottom section
+        let bottomStackView = UIStackView()
+        bottomStackView.axis = .vertical
+        bottomStackView.alignment = .fill
+        bottomStackView.distribution = .fillEqually
+        bottomStackView.spacing = 10
+        
+        // Create the grid of card buttons as in the previous example
         for _ in 0..<numberOfRows {
             let rowStackView = UIStackView()
             rowStackView.axis = .horizontal
@@ -41,23 +62,46 @@ class ViewController: UIViewController {
                 cardButton.setTitle("", for: .normal)
                 cardButton.backgroundColor = .red
                 
-                cardButton.addTarget(self, action: #selector(cardButtonTapped(_:)), for: .touchUpInside)
-                
                 rowStackView.addArrangedSubview(cardButton)
             }
             
-            mainStackView.addArrangedSubview(rowStackView)
+            bottomStackView.addArrangedSubview(rowStackView)
         }
         
-        view.addSubview(mainStackView)
+        // Create a parent stack view to arrange the top and bottom sections vertically
+        let parentStackView = UIStackView()
+        parentStackView.axis = .vertical
+        parentStackView.alignment = .fill
+        parentStackView.distribution = .fill
+        parentStackView.spacing = 20
         
-        mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        // Add the top and bottom stack views to the parent stack view
+        parentStackView.addArrangedSubview(topStackView)
+        parentStackView.addArrangedSubview(bottomStackView)
+        
+        // Add the parent stack view to the main view
+        view.addSubview(parentStackView)
+        
+        // Add constraints to define the position and size of the parent stack view
+        parentStackView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            mainStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
-            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            mainStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
-            
+            parentStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            parentStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            parentStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            parentStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20)
+        ])
+        
+        topStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topStackView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1)
+        ])
+        
+        bottomStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bottomStackView.topAnchor.constraint(equalTo: topStackView.bottomAnchor, constant: 20),
+            bottomStackView.leadingAnchor.constraint(equalTo: parentStackView.leadingAnchor),
+            bottomStackView.trailingAnchor.constraint(equalTo: parentStackView.trailingAnchor),
+            bottomStackView.bottomAnchor.constraint(equalTo: parentStackView.bottomAnchor)
         ])
         
     }
